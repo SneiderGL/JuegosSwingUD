7c0b3d65a971de7d8493dc0da93d1506a3ffe73f
SneiderGL <hsgrajalesl@udistrital.edu.co>
2025-12-01 00:51:06 -0500

    se creo el form y el java de las parejas locas, ademas se hizo la interfaz del form

---
+++ src/parejaslocas/MemoramaForm.java
@@ -0,0 +1,21 @@
+package parejaslocas;
+
+import javax.swing.*;
+
+public class MemoramaForm {
+    private JButton btn09;
+    private JButton btn01;
+    private JButton btn10;
+    private JButton btn11;
+    private JButton btn07;
+    private JButton btn06;
+    private JButton btn05;
+    private JButton btn08;
+    private JButton btn04;
+    private JButton btn03;
+    private JButton btn02;
+    private JButton btn00;
+    private JButton btnReiniciar;
+    private JPanel panelMain;
+    private JLabel lblEstado;
+}
